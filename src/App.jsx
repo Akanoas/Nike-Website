f86940aeccfc4f7f2231d764cc0455fee9dd8f4a
Abdoul Akano <akano1nassirou@gmail.com>
2023-11-17 04:56:38 -0500

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer,
+} from "./Sections";
+import { Nav } from "./Components";
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main className="relative ">
+      <Nav />
+      <section className="x1:padding-1 wide:padding-r padding-b ">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        {" "}
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer></SpecialOffer>
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        {" "}
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 }
