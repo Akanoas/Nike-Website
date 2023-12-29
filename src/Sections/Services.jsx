a16d65f8822a62287ca59c9684c779f7ce139ba5
Abdoul Akano <akano1nassirou@gmail.com>
2023-12-29 08:05:42 -0500

    super quality done

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,5 +1,13 @@
+import { services } from "../Constants";
+import ServiceCard from "../Components/ServiceCard";
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className=" max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
 
 export default Services;
